46c4835b9c9b8ec99fde9246bb59b6de1744622c
kodmm <53547701+kodmm@users.noreply.github.com>
2022-02-07 10:03:15 +0900

    pull develop

---
+++ pages/mypage/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+const MyPage: NextPage = () => {
+    return(
+        <div>
+            <p>Authenticated Successfully!!!</p>
+        </div>
+    )
+}
+export default MyPage;
