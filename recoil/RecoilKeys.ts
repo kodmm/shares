0b673f09c2594e52da8486893387f6ccde9c8c93
kodmm <53547701+kodmm@users.noreply.github.com>
2022-02-26 23:50:27 +0900

    defined atom key for WATCH_IS_WATCH

--- recoil/RecoilKeys.ts
+++ recoil/RecoilKeys.ts
@@ -3,6 +3,7 @@ export const RecoilAtomKeys = {
     TV_STATE: 'tvState',
     TV_STREAMING_STATE: 'tvStreamingState',
     WATCH_WATCH_STATE: 'watchWatchState',
+    WATCH_IS_WATCH: 'watchIsWatchState',
 } as const
 
 type RecoilAtomKeys = typeof RecoilAtomKeys[keyof typeof RecoilAtomKeys]
@@ -14,9 +15,10 @@ export const RecoilSelectorKeys = {
     TV_CREW: 'tvCrew',
     TV_STREAMING: 'tvStreaming',
     TV_BASEURL: 'tvBaseUrl',
+    WATCH_WATCH: 'watchWatch',
+    WATCH_VIDEO_ID: 'watchWatchVideoID',
+    WATCH_IS_WATCH: 'watchIsWatch',
 } as const
 
 type RecoilSelectorKeys = typeof RecoilSelectorKeys[keyof typeof RecoilSelectorKeys]
 
-    TV_STREAMING_STATE: 'tvStreamingState',
-    WATCH_WATCH: 'watchWatch',
