e9a3f95ff549db53a959f32ab4c5dc8519ff1050
kodmm <53547701+kodmm@users.noreply.github.com>
2022-02-27 00:06:06 +0900

    refactor to unnecessary import

--- api/tv.ts
+++ api/tv.ts
@@ -1,11 +1,3 @@
-import { useRecoilValue, useSetRecoilState } from'recoil';
-import { tvState } from '../recoil/atoms/tvState';
-import { tvStreamingState } from '../recoil/atoms/tvStreamingState';
-import { isWatchState } from '../recoil/atoms/watchState';
-import type { IStreamingIsWatch } from '../types/tvs/Tv'
-
-
-
 export const getStreamingIsWatch = (id: number) => {
     fetch(`http://localhost:3001/api/v1/tv/streaming/${id}/iswatch/`, {
         method: 'GET',
