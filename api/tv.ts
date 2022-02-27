4f907c2f991081bb7f989b314177c81a4aafb946
kodmm <53547701+kodmm@users.noreply.github.com>
2022-02-27 15:24:20 +0900

    add return value

--- api/tv.ts
+++ api/tv.ts
@@ -1,7 +1,9 @@
-export const getStreamingIsWatch = (id: number) => {
-    fetch(`http://localhost:3001/api/v1/tv/streaming/${id}/iswatch/`, {
+export const getStreamingIsWatch = async(id: number) => {
+    const resData: any = await fetch(`http://localhost:3001/api/v1/tv/streaming/${id}/iswatch/`, {
         method: 'GET',
         mode: 'cors',
         credentials: 'include',
     }).then(response => response.json())
+    
+    return resData
 }
