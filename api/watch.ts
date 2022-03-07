fc255d01d38cccd495425a1bc0a8e3201c0fbf38
kodmm <53547701+kodmm@users.noreply.github.com>
2022-03-08 04:10:44 +0900

    fixed how to write argument

--- api/watch.ts
+++ api/watch.ts
@@ -2,7 +2,7 @@ import type { IWatchData } from '../types/watches/watch';
 import type { IVideo } from '../types/watches/video';
 import type { IActor } from '../types/watches/actor';
 
-export const postWatch = async(data: { 'actors': IActor[], 'video':  IVideo, 'watch': IWatchData} ) => {
+export const postWatch = async(data: { actors: IActor[], video:  IVideo, watch: IWatchData} ) => {
     await fetch('http://localhost:3001/api/v1/watch', {
         mode: 'cors',
         method: 'POST',
