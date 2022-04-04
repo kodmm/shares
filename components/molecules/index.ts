1bb6eff269af6c1563d9e25a768dbc30ed04119f
kodmm <53547701+kodmm@users.noreply.github.com>
2022-04-05 04:13:40 +0900

    export Card, Navigation, StreamingServicesMypage Components

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,2 +1,6 @@
 export { BigWatchButton } from './bigWatchButton';
+export { Card } from './card';
+export { Navigation } from './navigation';
 export { StreamingServices } from './streamingServices';
+export { StreamingServicesMypage } from './streamingServicesMypage';
+
